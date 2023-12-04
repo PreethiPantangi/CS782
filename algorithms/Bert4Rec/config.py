261c4d02b8d46c85bb20d4a18813044c2ea0085d
aditya14896 <aditya14896@gmail.com>
2023-12-03 23:28:31 -0500

    Final commit

---
+++ algorithms/Bert4Rec/config.py
@@ -0,0 +1,4 @@
+RAW_DATASET_ROOT_FOLDER = './data/ml-1m'
+
+STATE_DICT_KEY = 'model_state_dict'
+OPTIMIZER_STATE_DICT_KEY = 'optimizer_state_dict'
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from algorithms.sasrec.main import SasRec
 from algorithms.caser.main import caser
+from algorithms.Bert4Rec.bert4rec import train
+from algorithms.Bert4Rec.runoptions import args as runoptions_args
+from algorithms.Bert4Rec.templates import set_template
 from datapreprocessing.datapreprocessing import datapreprocessing
 
 # Martians: Library for Sequential Recommendation algorithms SASRec, Caser, and Bert4Rec
@@ -19,6 +22,21 @@ class Recommendation:
 
     def caser(self, dataset):
         caser(dataset) 
+    
+    def bert4rec(self, datasetName):
+        datasetTocall = '1' if datasetName == 'MovieLens' else '2'
+        import argparse
+
+        # Define an argparse parser to parse command line arguments
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--template', type=str, default='train_bert')
+        parser.add_argument('--dataset_code', type=str, default=datasetTocall)
+        parser.add_argument('--train_negative_sampling_seed', type=int, default=0)
+        parser.add_argument('--enable_lr_schedule', type=bool, default=True)
+
+        args = parser.parse_args(namespace=runoptions_args)
+        set_template(args)
+        train() 
 
 if __name__ == '__main__':
     dataset = 'beauty'
@@ -27,3 +45,4 @@ if __name__ == '__main__':
     recommendation = Recommendation()
     recommendation.sasrec(dataset)
     # recommendation.caser(dataset)
+    # recommendation.bert4rec(dataset)
